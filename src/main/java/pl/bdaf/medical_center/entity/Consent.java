5be021bb8ec42d07b2f4332adede4d183a9253cf
bdaf <kamilkorenkiewiczpw@gmail.com>
2021-12-07 12:32:29 +0100

    Consent entity added

---
+++ src/main/java/pl/bdaf/medical_center/entity/Consent.java
@@ -0,0 +1,30 @@
+package pl.bdaf.medical_center.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "consent")
+public class Consent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "consent_sequence")
+    @SequenceGenerator(name = "consent_sequence", sequenceName = "consent_sequence", allocationSize = 1)
+    @Column(name = "consent_id", nullable = false)
+    private Long consentId;
+
+    @ManyToOne
+    @JoinColumn(name = "patient_id", referencedColumnName = "patient_id")
+    private Patient patient;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id", referencedColumnName = "project_id")
+    private ResearchProject researchProject;
+}
