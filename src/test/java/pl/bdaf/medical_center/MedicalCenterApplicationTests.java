a82538d340483de8458c7d4250a0e169f24ede21
bdaf <kamilkorenkiewiczpw@gmail.com>
2021-12-07 11:55:23 +0100

    Initial spring boot project commit

---
+++ src/test/java/pl/bdaf/medical_center/MedicalCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bdaf.medical_center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
